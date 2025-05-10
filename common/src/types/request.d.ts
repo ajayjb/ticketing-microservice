e4b186dc6900dfc18ef8d3a9b7d4a5aa1cf41537
ajayjb <ajayjb11@gmail.com>
2025-05-11 03:29:20 +0530

    updates

--- common/src/types/request.d.ts
+++ common/src/types/request.d.ts
@@ -1,10 +1,11 @@
+import { JwtPayload } from "@/services/jwt.service";
 import { Session } from "cookie-session";
 
 declare global {
   namespace Express {
     interface Request {
       session: Session & { token: string };
-      user?: UserType;
+      user?: JwtPayload;
     }
   }
 }
