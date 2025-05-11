a4391f068b1d33c45f24c1699cef824692a11e62
ajayjb <ajayjb11@gmail.com>
2025-05-12 02:31:28 +0530

    updates

--- common/tsup.config.ts
+++ common/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   entry: ['src/index.ts'], // your entry file
   format: ['esm', 'cjs'],  // output both module formats
   dts: true,               // generate .d.ts files
-  sourcemap: true,         // optional, for debugging
+  sourcemap: false,         // optional, for debugging
   clean: true,             // clean output before build
   target: 'es2022',        // or 'es2020', depending on your Node version
 })
