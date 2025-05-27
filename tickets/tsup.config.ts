6278770885ee3a598ba2491f6a6825645221bf09
ajayjb <ajayjb11@gmail.com>
2025-05-28 00:56:59 +0530

    Feat: Tickets

--- tickets/tsup.config.ts
+++ tickets/tsup.config.ts
@@ -1,11 +1,11 @@
 // tsup.config.ts
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/server.ts'], // your entry file
-  format: ['esm', 'cjs'],  // output both module formats
-  dts: true,               // generate .d.ts files
-  sourcemap: true,         // optional, for debugging
-  clean: true,             // clean output before build
-  target: 'es2022',        // or 'es2020', depending on your Node version
-})
+  entry: ["src/server.ts"], // your entry file
+  format: ["esm", "cjs"], // output both module formats
+  dts: true, // generate .d.ts files
+  sourcemap: true, // optional, for debugging
+  clean: true, // clean output before build
+  target: "es2022", // or 'es2020', depending on your Node version
+});
