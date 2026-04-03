dbd0afabfd46fe46fc47303f16c9eac08da8bc2c
ajayjb <ajayjb11@gmail.com>
2026-04-04 05:01:11 +0530

    refactor: remove startup console logs across all microservices

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -3,7 +3,6 @@ import { sanitizedConfig } from "@/config/config";
 import app from "@/app";
 
 const startApp = async () => {
-  console.log("Starting auth microservice...")
   app.init(sanitizedConfig.PORT);
 };
 
